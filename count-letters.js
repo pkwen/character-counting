53a7fabf07ee1620c5e9a2162b80c1edde79f967
Danny K Pan <kaiwen006@gmail.com>
2018-01-16 22:34:03 +0000

    completed function returning expected results

--- count-letters.js
+++ count-letters.js
@@ -1,3 +1,13 @@
 function countLetters(str) {
-
+  var count = {};
+  str.toLowerCase().split(" ").join("").split("").forEach(function(char) {
+    if(count[char] !== undefined) {
+      count[char]++;
+    }else {
+      count[char] = 1;
+    }
+  });
+  return count;
 }
+
+console.log(countLetters("I wish there would be quality pizza at the JS meet"));
